05a1dc86b6f285c5d4090c9178a0a7b052f82fc9
Rawand <62562422+rawand-faraidun@users.noreply.github.com>
2023-10-17 02:36:15 +0300

    serving client

--- src/server/routes/router.ts
+++ src/server/routes/router.ts
@@ -1,12 +1,20 @@
-import { Router } from 'express'
+import { resolve } from 'path'
+import express from 'express'
+import { dirname } from 'dirname-filename-esm'
 
-const router = Router()
+const __dirname = dirname(import.meta)
 
-/**
- * 404
- */
-router.use((req, res) => {
-  res.status(404).send('404 Not Found')
+// this directory path is based on `dist` folder after build
+const clientDir = resolve(__dirname, 'client')
+
+const router = express.Router()
+
+// static client
+router.use(express.static(clientDir))
+
+// client ui
+router.get('*', (req, res) => {
+  res.sendFile(resolve(clientDir, 'index.html'))
 })
 
 export default router
