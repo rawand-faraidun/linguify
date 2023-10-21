61f5cb0cb429c6c850a2217477d564fbaf04717f
Rawand <62562422+rawand-faraidun@users.noreply.github.com>
2023-10-22 00:16:53 +0300

    linguify validation

---
+++ lib/linguifyValidation.ts
@@ -0,0 +1,63 @@
+import { existsSync, statSync } from 'fs'
+import { resolve } from 'path'
+import chalk from 'chalk'
+import { configPath, getUserConfig, rootPath } from './utils'
+
+/**
+ * validates linguify config
+ */
+export const linguifyValidation = () => {
+  try {
+    // checking if the config file excists
+    if (!existsSync(configPath)) {
+      throw new Error(`Linguify config file is not found, initiate linguify with '${chalk.underline('linguify init')}'`)
+    }
+
+    // user config
+    const config = getUserConfig()
+
+    // checking the user config localesPath
+    if (!config.localesPath) {
+      throw new Error(chalk.yellow(`Linguify config file misses 'localesPath' key, please add it before starting`))
+    }
+    if (typeof config.localesPath != 'string') {
+      throw new Error(chalk.yellow(`Provided 'localesPath' is not a string, please change it before starting`))
+    }
+    if (!existsSync(resolve(rootPath, config.localesPath))) {
+      throw new Error(chalk.yellow(`Provided 'localesPath' is not found, please create the directory before starting`))
+    }
+    if (!statSync(resolve(rootPath, config.localesPath)).isDirectory()) {
+      throw new Error(chalk.yellow(`Provided 'localesPath' is not a directory, please change it before starting`))
+    }
+
+    // checking locales
+    if (!config.locales) {
+      throw new Error(chalk.yellow(`Linguify config file misses 'locales' key, please add it before starting`))
+    }
+    if (!Array.isArray(config.locales)) {
+      throw new Error(chalk.yellow(`Provided 'locales' is not an array, please change it before starting`))
+    }
+    if (config.locales.some(locale => typeof locale != 'string')) {
+      throw new Error(chalk.yellow(`Provided 'locales' is not an array of strings, please change it before starting`))
+    }
+    if (config.locales.length == 0) {
+      throw new Error(chalk.yellow(`Provided 'locales' array is empty, please change it before starting`))
+    }
+
+    // checking default locale
+    if (!config.defaultLocale) {
+      throw new Error(chalk.yellow(`Linguify config file misses 'defaultLocale' key, please add it before starting`))
+    }
+    if (typeof config.defaultLocale != 'string') {
+      throw new Error(chalk.yellow(`Provided 'defaultLocale' is not a string, please change it before starting`))
+    }
+    if (!config.locales.includes(config.defaultLocale)) {
+      throw new Error(
+        chalk.yellow(`Provided 'defaultLocale' is not included in in 'locales', please change it before starting`)
+      )
+    }
+  } catch (error: any) {
+    console.error(chalk.red(error.message))
+    process.exit(0)
+  }
+}
--- src/start.ts
+++ src/start.ts
@@ -1,10 +1,10 @@
-import { existsSync, statSync, watch } from 'fs'
-import { resolve } from 'path'
+import { watch } from 'fs'
 import chalk from 'chalk'
 import startServer from './server/server'
 import { defaultPort } from '@lib/defaults'
+import { linguifyValidation } from '@lib/linguifyValidation'
 import { syncNamespaces } from '@lib/syncNamespaces'
-import { configPath, getUserConfig, rootPath } from '@lib/utils'
+import { configPath } from '@lib/utils'
 
 /**
  * start linguify command
@@ -13,63 +13,17 @@ import { configPath, getUserConfig, rootPath } from '@lib/utils'
  */
 const start = async (port: number = defaultPort) => {
   try {
-    // user config
-    const config = getUserConfig()
+    // validating linguify configs
+    linguifyValidation()
 
-    // checking if the config file excists
-    if (!existsSync(configPath)) {
-      throw new Error(`Linguify config file is not found, initiate linguify with '${chalk.underline('linguify init')}'`)
-    }
-
-    // checking the user config localesPath
-    if (!config.localesPath) {
-      throw new Error(chalk.yellow(`Linguify config file misses 'localesPath' key, please add it before starting`))
-    }
-    if (typeof config.localesPath != 'string') {
-      throw new Error(chalk.yellow(`Provided 'localesPath' is not a string, please change it before starting`))
-    }
-    if (!existsSync(resolve(rootPath, config.localesPath))) {
-      throw new Error(chalk.yellow(`Provided 'localesPath' is not found, please create the directory before starting`))
-    }
-    if (!statSync(resolve(rootPath, config.localesPath)).isDirectory()) {
-      throw new Error(chalk.yellow(`Provided 'localesPath' is not a directory, please change it before starting`))
-    }
-
-    // checking locales
-    if (!config.locales) {
-      throw new Error(chalk.yellow(`Linguify config file misses 'locales' key, please add it before starting`))
-    }
-    if (!Array.isArray(config.locales)) {
-      throw new Error(chalk.yellow(`Provided 'locales' is not an array, please change it before starting`))
-    }
-    if (config.locales.some(locale => typeof locale != 'string')) {
-      throw new Error(chalk.yellow(`Provided 'locales' is not an array of strings, please change it before starting`))
-    }
-    if (config.locales.length == 0) {
-      throw new Error(chalk.yellow(`Provided 'locales' array is empty, please change it before starting`))
-    }
-
-    // checking default locale
-    if (!config.defaultLocale) {
-      throw new Error(chalk.yellow(`Linguify config file misses 'defaultLocale' key, please add it before starting`))
-    }
-    if (typeof config.defaultLocale != 'string') {
-      throw new Error(chalk.yellow(`Provided 'defaultLocale' is not a string, please change it before starting`))
-    }
-    if (!config.locales.includes(config.defaultLocale)) {
-      throw new Error(
-        chalk.yellow(`Provided 'localesPath' is not included in in 'locales', please change it before starting`)
-      )
-    }
+    // syncing namespaces
+    syncNamespaces()
 
     // notifying user about config
     console.log(`Reading linguify config from ${chalk.cyan(chalk.underline(configPath))}`)
 
     // advising to not change locale and namespaces locale
     console.log(chalk.yellow(`Please avoid interacting with locales and namespaces manually while using linguify`))
-
-    // syncing namespaces
-    syncNamespaces()
 
     // starting linguify server
     startServer(port)
