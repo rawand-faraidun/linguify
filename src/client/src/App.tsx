1f1c41234d1f636ad94fa817329ad5eb68878967
Rawand <62562422+rawand-faraidun@users.noreply.github.com>
2023-10-21 03:04:10 +0300

    header

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Header from './components/App/Header'
 import AppProvider from './components/context/AppContext'
 import IndexPage from './pages/Index'
 
@@ -7,6 +8,8 @@ function App() {
     <>
       <AppProvider>
         <BrowserRouter>
+          <Header />
+
           <Routes>
             <Route path="/" element={<IndexPage />} />
           </Routes>
---
+++ src/client/src/components/App/Theme.tsx
@@ -0,0 +1,49 @@
+import { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
+import Svg from '../Svg'
+import { Button } from '../ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger
+} from '../ui/dropdown-menu'
+import { Theme as ThemeColor } from '@lib/types/Theme'
+
+const Theme = () => {
+  const { theme, setTheme, currentTheme } = useContext(AppContext)
+
+  console.log(currentTheme)
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost">
+          <Svg
+            paths={
+              currentTheme == 'light'
+                ? [
+                    'M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z'
+                  ]
+                : [
+                    'M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z',
+                    'M17 4a2 2 0 0 0 2 2a2 2 0 0 0 -2 2a2 2 0 0 0 -2 -2a2 2 0 0 0 2 -2',
+                    'M19 11h2m-1 -1v2'
+                  ]
+            }
+          />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="z-high" align="end">
+        <DropdownMenuRadioGroup value={theme} onValueChange={v => setTheme(v as ThemeColor)}>
+          <DropdownMenuRadioItem value="system">System</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="dark">Dark</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="light">Light</DropdownMenuRadioItem>
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+export default Theme
