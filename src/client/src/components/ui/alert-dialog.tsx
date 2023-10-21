72f9ed69be7e1ce9be9a008c0d2f820d0d3c38a1
Rawand <62562422+rawand-faraidun@users.noreply.github.com>
2023-10-22 00:29:37 +0300

    fix alert dialog button import

--- src/client/src/components/ui/alert-dialog.tsx
+++ src/client/src/components/ui/alert-dialog.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import * as AlertDialogPrimitive from '@radix-ui/react-alert-dialog'
-import { buttonVariants } from 'src/components/ui/button'
+import { buttonVariants } from './button'
 import { cn } from '@lib/functions/cn'
 
 const AlertDialog = AlertDialogPrimitive.Root
